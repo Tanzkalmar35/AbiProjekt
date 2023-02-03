edc00c14d8c2fc72e946d68292098ce28b0c5cb0
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-02-03 14:27:37 +0100

    Every Great Story starts with a big joint and some great food

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Donute_chart from "./diagramm_components/donute/donute_chart.jsx"; 
 
 const App = () => {
   return (
     <div className="">
       <p className="">new App</p>
+      <Donute_chart />
     </div>
   );
 };
---
+++ src/diagramm_components/donute/donute_chart.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Donute_chart = () => {
+  return (
+    <div>Donute Chart</div>
+  );
+};
+
+export default Donute_chart;
