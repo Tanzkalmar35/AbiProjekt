18a2300b9e17b61e17432c58bdabd1baeb19f8ed
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-02-04 15:45:07 +0100

    text in chart middle

--- src/diagramm_components/donute/donute_chart.jsx
+++ src/diagramm_components/donute/donute_chart.jsx
@@ -3,15 +3,30 @@ import { useState } from "react";
 import { Doughnut } from "react-chartjs-2";
 import { Chart as chartjs, ArcElement, Tooltip, Legend } from "chart.js/auto";
 
-
 const Donute_chart = () => {
   chartjs.register(ArcElement, Tooltip, Legend);
 
   const [middle, setMiddle] = useState("he");
 
+  const textCenter = {
+    id: "textCenter",
+    beforeDatasetsDraw(chart, args, pluginOptions) {
+      const { ctx, data } = chart;
+      ctx.save();
+      ctx.font = "bolder 30px sans-serif";
+      ctx.fillStyle = "black";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText(
+        "hallo",
+        chart.getDatasetMeta(0).data[0].x,
+        chart.getDatasetMeta(0).data[0].y
+      );
+    },
+  };
+
   const [data, setData] = React.useState({
     labels: ["O2", "N2", "H2O", "CO2"],
-
     datasets: [
       {
         data: [20, 78, 10, 4],
@@ -28,7 +43,7 @@ const Donute_chart = () => {
 
   return (
     <div className="w-screen h-screen flex justify-center">
-      <Doughnut data={data} height="100" width="100" onClick={(value) => {console.log(value.value)}} />
+      <Doughnut data={data} plugins={[textCenter]} />
     </div>
   );
 };
