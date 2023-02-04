10dbd11dc223af2847f0216085f4ad7bd2336108
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-02-04 15:08:08 +0100

    Pie chart do be funky

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import "./App.css";
 import Donute_chart from "./diagramm_components/donute/donute_chart.jsx"; 
-import {TestData} from "./diagramm_components/TestData.jsx"
 
 const App = () => {
   
-  const [sensor_data, setsensor_data] = React.useState({
-  type :  "doughnut",
-  data : TestData,
-  });
-
+ 
 
   
   return (
     <div className="">
-      <p className="">new App</p>
-      <Donute_chart data={sensor_data} />
+      <Donute_chart></Donute_chart>
     </div>
   );
 };
--- src/diagramm_components/donute/donute_chart.jsx
+++ src/diagramm_components/donute/donute_chart.jsx
@@ -1,10 +1,36 @@
 import React from "react";
-import {Pie} from "react-chartjs-2";
-import {Chart as chartjs} from "chart.js/auto" 
+import { useState } from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as chartjs, ArcElement, Tooltip, Legend } from "chart.js/auto";
 
-const Donute_chart = ({data}) => {
-  return <Pie data={data} />;
-  
+
+const Donute_chart = () => {
+  chartjs.register(ArcElement, Tooltip, Legend);
+
+  const [middle, setMiddle] = useState("he");
+
+  const [data, setData] = React.useState({
+    labels: ["O2", "N2", "H2O", "CO2"],
+
+    datasets: [
+      {
+        data: [20, 78, 10, 4],
+        backgroundColor: [
+          "rgba(101, 147, 245)",
+          "rgba(0, 0, 128)",
+          "rgb(16 52 166)",
+          "rgb(0 49 80)",
+        ],
+        borderColor: "black",
+      },
+    ],
+  });
+
+  return (
+    <div className="w-screen h-screen flex justify-center">
+      <Doughnut data={data} height="100" width="100" onClick={(value) => {console.log(value.value)}} />
+    </div>
+  );
 };
 
 export default Donute_chart;
