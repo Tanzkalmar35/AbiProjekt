b5db7fb51ce9d1c72e4fb59c77acfdcd41ca6ede
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-02-05 18:26:32 +0100

    Commentet the code for readers

--- src/diagramm_components/line/line_chart.jsx
+++ src/diagramm_components/line/line_chart.jsx
@@ -5,62 +5,61 @@ import { Chart as chartjs, ArcElement, Tooltip, Legend } from "chart.js/auto";
 import { co2_pre } from "../../Logic/formals";
 
 const Line_chart = ({ trigger }) => {
+  //Alway get the current time as an array
   const [time, setTime] = useState(get_time);
-  const [current_data, setCurrentData] = useState([0.04, 0.04, 0.03, 0.05, 0.03]);
+
+  //This date will be pulled from Firebase
+  const [current_data, setCurrentData] = useState([
+
+    0.04, 0.04, 0.03, 0.05, 0.03,
+
+  ]);
+
+  //Standart layout for chart data
   const [chart, setChart] = useState({
+
+    //Those are the stamps under the chart
     labels: time,
     datasets: [
       {
+        //Those are the points in the chart
         data: current_data,
-        backgroundColor: [
-          "rgba(101, 147, 245)",
-          "rgba(0, 0, 128)",
-          "rgb(16 52 166)",
-          "rgb(0 49 80)",
-        ],
-        borderColor: "black",
+
       },
     ],
   });
-  //const [] = useState()
+
+  //update the chart dynamically
 
   setTimeout(() => {
     setTime(get_time);
     setCurrentData(get_Data(current_data));
-    
+
     setChart({
       labels: time,
       datasets: [
         {
           data: current_data,
-          backgroundColor: [
-            "rgba(101, 147, 245)",
-            "rgba(0, 0, 128)",
-            "rgb(16 52 166)",
-            "rgb(0 49 80)",
-          ],
-          borderColor: "black",
         },
       ],
     });
-  }, 60000)
+  }, 60000);
 
-  
-  
+  //This function is deleting the first element of the array and adds a new one at the end
+
   function get_Data(data) {
     let next = current_data;
-    
-    
-    const remove = next.splice(0,1);
+
+    const remove = next.splice(0, 1);
     next.push(Math.random() * (0.05 - 0.03) + 0.05);
     console.log(next);
-    
+
     return next;
-  
-  
   }
 
-  
+  /*THis function is getting the current time and makes a array where the first 
+  element is the current time and following elements 
+  are the current data + minutes*/
 
   function get_time() {
     const date = new Date();
@@ -75,6 +74,8 @@ const Line_chart = ({ trigger }) => {
       hours + ":" + (minutes + 4),
     ];
   }
+
+  //Displays the chart if the trigger is true
 
   return trigger ? (
     <div className="w-3/6 h-3/6 flex justify-center align-middle">
