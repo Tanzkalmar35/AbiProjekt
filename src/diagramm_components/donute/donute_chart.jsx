11d523226173d07f993a72dd38f1212a24d0e634
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-02-07 16:24:57 +0100

    Update donute_chart.jsx

--- src/diagramm_components/donute/donute_chart.jsx
+++ src/diagramm_components/donute/donute_chart.jsx
@@ -5,11 +5,9 @@ import { Doughnut } from "react-chartjs-2";
 import { Chart as chartjs, ArcElement, Tooltip, Legend } from "chart.js/auto";
 import { add_vorlage, new_data_overall } from "../../db";
 
-
-
 const Donute_chart = ({ trigger }) => {
   chartjs.register(ArcElement, Tooltip, Legend);
- 
+
   //Draws the Text in the middel can't be changed (later maybe but right now its just "AIR QUALITY CHART")
   const textCenter = {
     id: "textCenter",
@@ -28,12 +26,30 @@ const Donute_chart = ({ trigger }) => {
     },
   };
 
-  let CO2 = 0.04;
+  const options = {
+    Tooltips: {
+      callbacks: {
+        label: (tooltipItem, data) => {
+          return (
+            data.labels[tooltipItem.index] +
+            ": " +
+            data.datasets[0].data[tooltipItem.index] + "sus"
+          );
+        },
+      },
+    },
+  };
+
+  /*
+  Normal levels
+  N2 = 0.78
+  O2 = 0.20
+  CO2 = 0.0004
+  */
+  let CO2 = 0.0004;
   let O2 = 0.1;
   let H20 = 0.1;
   let N2 = 0.78;
-
-  add_vorlage();
 
   //Sample data for the chart, later will be replaced with actual data from Firebase
   const [data, setData] = React.useState({
@@ -42,7 +58,7 @@ const Donute_chart = ({ trigger }) => {
     labels: ["O2", "N2", "H2O", "CO2"],
     datasets: [
       {
-        data: [O2, N2, H20, CO2],
+        data: [O2, N2, H20, CO2 * 100],
         backgroundColor: [
           "rgba(101, 147, 245)",
           "rgba(0, 0, 128)",
@@ -59,10 +75,10 @@ const Donute_chart = ({ trigger }) => {
     <div className="w-screen h-screen flex justify-center">
       <Doughnut
         data={data}
-        
         plugins={[textCenter]}
         height="100"
         width="100"
+        options={[options]}
       />
     </div>
   ) : null;
