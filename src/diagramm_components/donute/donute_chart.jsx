85bde7711ceb5022e4344a89e696a9b938817cc6
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-02-09 10:03:14 +0100

    https://media.discordapp.net/attachments/1064504529233641473/1073166934805331978/7ainbi.jpg

--- src/diagramm_components/donute/donute_chart.jsx
+++ src/diagramm_components/donute/donute_chart.jsx
@@ -7,7 +7,17 @@ import { add_vorlage, new_data_overall, get_current_data } from "../../db";
 
 const Donute_chart = ({ trigger }) => {
   chartjs.register(ArcElement, Tooltip, Legend);
-  
+  /*
+  Normal levels
+  N2 = 0.78
+  O2 = 0.20
+  CO2 = 0.0004
+  */
+  let CO2 = 0.0004;
+  let O2 = 0.1;
+  let H20 = 0.1;
+  let N2 = 0.78;
+
   //Draws the Text in the middel can't be changed (later maybe but right now its just "AIR QUALITY CHART")
   const textCenter = {
     id: "textCenter",
@@ -25,36 +35,40 @@ const Donute_chart = ({ trigger }) => {
       );
     },
   };
- 
-  const options = {
-    Tooltips: {
-      callbacks: {
-        label: (tooltipItem, data) => {
-          return (
-            data.labels[tooltipItem.index] +
-            ": " +
-            data.datasets[0].data[tooltipItem.index] + "sus"
-          );
+
+  const [chart_option, setChartOptions] = useState({
+    options: {
+      plugins: {
+        legend: {
+          display: true,
+          position: "bottom",
+          labels: { padding: 25 },
+        },
+        tooltip: {
+          callbacks: {
+            label: (context) => {
+              console.log(context);
+              if (context.label == "CO2") {
+                return CO2 * 100 + "%";
+              } else {
+                return context.parsed * 100 + "%";
+              }
+            },
+          },
+        },
+        title: {
+          display: true,
+          text: "H2O : 0.1 | O2 : 0.1 | N2 : 0.78 | CO2 < 0.1",
+          font: { size: 25 },
         },
       },
     },
-  };
-
-  /*
-  Normal levels
-  N2 = 0.78
-  O2 = 0.20
-  CO2 = 0.0004
-  */
-  let CO2 = 0.0004;
-  let O2 = 0.1;
-  let H20 = 0.1;
-  let N2 = 0.78;
+  });
 
   //Sample data for the chart, later will be replaced with actual data from Firebase
   const [data, setData] = React.useState({
     //this will be late read from the database
-
+    type: "doughnut",
     labels: ["O2", "N2", "H2O", "CO2"],
     datasets: [
       {
@@ -66,6 +80,7 @@ const Donute_chart = ({ trigger }) => {
           "rgb(0 49 80)",
         ],
         borderColor: "black",
+        hoverOffset: 20,
       },
     ],
   });
@@ -75,10 +90,10 @@ const Donute_chart = ({ trigger }) => {
     <div className="w-screen h-screen flex justify-center">
       <Doughnut
         data={data}
+        options={chart_option.options}
         plugins={[textCenter]}
         height="100"
         width="100"
-        options={[options]}
       />
     </div>
   ) : null;
