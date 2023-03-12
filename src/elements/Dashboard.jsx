084e4fed9fdf86aa5f9397263f31518bb548c750
CreapZ <jarnocallies@gmail.com>
2023-03-12 12:57:15 +0100

    Revert "Bruch Tabelle"
    This reverts commit fd0da71c787b2df787f0ef4e3836d2d1d9299d4a.

--- src/elements/Dashboard.jsx
+++ src/elements/Dashboard.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import Donute_chart from "../diagramm_components/donute/donute_chart"
 import Bar_chart from "../diagramm_components/bar/bar_chart"
 import Line_chart from "../diagramm_components/line/line_chart"
-import table from "../diagramm_components/table/table"
 
 import { useState } from "react"
 
@@ -11,8 +10,6 @@ const Dashboard = () => {
   //Changes the visibility of the chart
   const [doughnut_chrat_view, setDoughnut_chrat_view] = useState(true);
   const [bar_chrat_view, setbar_chrat_view] = useState(false);
-  const [table_view, setbar_table_view] = useState(false);
-
 
   const [line_chrat_view, setline_chrat_view] = useState(false);
 
@@ -22,7 +19,6 @@ const Dashboard = () => {
       <Donute_chart trigger={doughnut_chrat_view}></Donute_chart>
       <Bar_chart trigger={bar_chrat_view}></Bar_chart>
       <Line_chart trigger={line_chrat_view}></Line_chart>
-      <table trigger={table_view}></table>
     </div>
   );
 }
