ada5501e300ac0dd8d18fa930d03938651a38d67
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-03-13 19:30:51 +0100

    need to add right data for status

--- src/single_charts/Funticons_for_charts.js
+++ src/single_charts/Funticons_for_charts.js
@@ -1,1 +1,5 @@
-
+export function check(status) {
+    if(status == "Good"){
+        return "bg-green-500";
+    }else {return "bg-red-500";}
+}
--- src/single_charts/Table.jsx
+++ src/single_charts/Table.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { check } from "./Funticons_for_charts";
 
 export default function Table({ Data }) {
     
-    const [o2_level, setO2_level] = React.useState("Good");
-    const [N2_level, setN2_level] = React.useState("Good");
-    const [Co2_level, setCO2_level] = React.useState("Good");
-    const [H2O_level, setH2O_level] = React.useState("Good");
+    const [o2_level, setO2_level] = React.useState("");
+    const [N2_level, setN2_level] = React.useState("");
+    const [Co2_level, setCO2_level] = React.useState("");
+    const [H2O_level, setH2O_level] = React.useState("");
 
 
 
@@ -17,27 +18,27 @@ export default function Table({ Data }) {
 
     React.useEffect(() => {
         if(Data[1] > 0.235 || Data[1] < 0.195){
-            setO2_level("Bad");
+          setN2_level("Bad");
     
-    }else{setO2_level("Good")}},[Data[1]]);
+    }else{setN2_level("Good")}},[Data[1]]);
 
     React.useEffect(() => {
         if(Data[2] > 0.235 || Data[2] < 0.195){
-            setO2_level("Bad");
+          setCO2_level("Bad");
     
-    }else{setO2_level("Good")}},[Data[2]]);
+    }else{setCO2_level("Good")}},[Data[2]]);
   
     React.useEffect(() => {
         if(Data[3] > 0.235 || Data[3] < 0.195){
-            setO2_level("Bad");
+          setH2O_level("Bad");
     
-    }else{setO2_level("Good")}},[Data[3]]);
+    }else{setH2O_level("Good")}},[Data[3]]);
   
     return (
-    <div className=" grid-cols-3 flex">
-      <ul className="col-1">
-        <li>O2: </li>
-        <li>N2: </li>
+    <div className=" grid-cols-3 flex justify-between">
+      <ul className="col-1 pr-10">
+        <li>O2:  </li>
+        <li>N2:  </li>
         <li>CO2: </li>
         <li>H2O: </li>
         
@@ -49,11 +50,11 @@ export default function Table({ Data }) {
         <li>{Data[3]}</li>
        
       </ul>
-      <ul className="col-3">
-        <li>{o2_level}</li>
-        <li>{N2_level}</li>
-        <li>{Co2_level}</li>
-        <li>{H2O_level}</li>
+      <ul className="pl-10 col-3">
+        <li className={check(o2_level)}>{o2_level}</li>
+        <li className={check(N2_level)}>{N2_level}</li>
+        <li className={check(Co2_level)}>{Co2_level}</li>
+        <li className={check(H2O_level)}>{H2O_level}</li>
 
         
       </ul>
