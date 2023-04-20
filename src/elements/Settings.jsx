85269cc750dee36ef7b3e9d0388203f0b3e54057
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-04-20 14:48:52 +0200

    Startet with the Settings

--- src/elements/Settings.jsx
+++ src/elements/Settings.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <div className='text-5xl text-white'>
+      <table>
+        <tbody>
+          <tr>
+            <td>Dark Mode</td>
+            <td>Button later here</td>
+          </tr>
+          <tr>
+            <td>Benachrichtigungen</td>
+            <td><button>Here will be more</button></td>
+          </tr>
+          <tr>
+            <td>Connected Units</td>
+            <td><button>How to?</button></td>
+          </tr>
+          <tr>
+            <td>Help</td>
+            <td><button>Help will be added later</button></td>
+          </tr>
+          <tr>
+            <td>About Us</td>
+            <td><button>Among US</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
