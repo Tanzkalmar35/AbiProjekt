1fdc718ff167239a34f1d6a9c821be1e8cd59982
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-04-23 09:26:35 +0200

    creating the foundation for light/dark mode toggle

---
+++ src/Mode.js
@@ -0,0 +1,12 @@
+/**
+ * Used to switch between light and dark mode easily.
+ * Call these styles with a check whether dark/light mode is activated
+ * @type {{light: string, dark: string}}
+ */
+export const darkMode = {
+    color1: "",
+}
+
+export const lightMode = {
+    color1: "",
+}
