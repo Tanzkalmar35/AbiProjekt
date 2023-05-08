6ae376d37ad3206427a919eef348713f3e57982f
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-05-08 07:54:13 +0200

    Updating Profileinfo

--- src/UiComponents/Personinfo.jsx
+++ src/UiComponents/Personinfo.jsx
@@ -15,7 +15,7 @@ const Personinfo = ({Name, PersonalInfo, Picture }) => {
     <div className="">
         <div className="card card-side bg-base-100 shadow-xl">
 
-            <figure><img src={Picture} className="w-[150px] h-auto" /></figure>
+            <figure><img alt="profile picture" src={Picture} className="w-[200px] h-[100%]" /></figure>
             <div className="card-body text-left">
                 <h2 className="card-title">{Name}</h2>
                 <div>{result.map(element => <p>{element}</p>)}</div>
--- src/elements/Info.jsx
+++ src/elements/Info.jsx
@@ -2,20 +2,23 @@ import React from "react";
 import Personinfo from "../UiComponents/Personinfo.jsx";
 
 const Info = () =>{
+
+
+
     return (
         <div className="flex justify-center items-center w-[100%] h-[100%]">
             <table className="w-[100%] h-[100%] text-center flex justify-center items-center">
                <tbody>
                <tr>
-                   <td className="pl-5 pr-5"><Personinfo PersonalInfo={["Age: 18", "Hobbys: Codding, Reading", ""]} Name={"Nick Hillmann"} Picture={"/src/assets/NickProf.jpeg"}></Personinfo>
+                   <td className="pl-5 pr-5"><Personinfo PersonalInfo={["Age: 18", "Tasks:", " Builden the WebApp,", "Setting up the Arduino,", "Analyse the Data for the Charts", "Role: Programmer"]} Name={"Nick Hillmann"} Picture={"/src/assets/Nick03.jpeg"}></Personinfo>
                    </td>
-                   <td className="pl-5 pr-5"><Personinfo PersonalInfo={""} Name={"Jarno Calise"} Picture={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/2017-05-14_NRW_Landtagswahl_by_Olaf_Kosinsky-116.jpg/220px-2017-05-14_NRW_Landtagswahl_by_Olaf_Kosinsky-116.jpg"}></Personinfo>
+                   <td className="pl-5 pr-5"><Personinfo PersonalInfo={["Age: 18", "Tasks:", "Dokumentation,", "Prognosefunktion (Berechnung),", "Design Templates", "Role: Teamleader"]} Name={"Jarno Calise"} Picture={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/2017-05-14_NRW_Landtagswahl_by_Olaf_Kosinsky-116.jpg/220px-2017-05-14_NRW_Landtagswahl_by_Olaf_Kosinsky-116.jpg"}></Personinfo>
                    </td>
                </tr>
                <tr>
-                   <td className="pl-5 pr-5"><Personinfo PersonalInfo={""} Name={"Marc Müller"} Picture={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/2017-05-14_NRW_Landtagswahl_by_Olaf_Kosinsky-116.jpg/220px-2017-05-14_NRW_Landtagswahl_by_Olaf_Kosinsky-116.jpg"}></Personinfo>
+                   <td className="pl-5 pr-5"><Personinfo PersonalInfo={["Age: 21", "Tasks:", "Marketing,", "Design,", "Investor", "Role: Finazmanger"]} Name={"Marc Müller"} Picture={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/2017-05-14_NRW_Landtagswahl_by_Olaf_Kosinsky-116.jpg/220px-2017-05-14_NRW_Landtagswahl_by_Olaf_Kosinsky-116.jpg"}></Personinfo>
                    </td>
-                   <td className="pl-5 pr-5"><Personinfo PersonalInfo={["Age: 18,", "Hobbies: coding, table-tennis", "Role in this team: programmer"]} Name={"Fabian Holler"} Picture={"/src/assets/FabianProf.png"}></Personinfo>
+                   <td className="pl-5 pr-5"><Personinfo PersonalInfo={["Age: 18,", "Tasks:" ,"coding, ","table-tennis", "Role in this team: programmer"]} Name={"Fabian Holler"} Picture={"/src/assets/FabianProf.png"}></Personinfo>
                    </td>
                </tr>
                </tbody>
