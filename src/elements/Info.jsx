2e9458292651d3d1a6061c63a7dc008d0a3605a9
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-05-30 13:06:05 +0200

    ProfBilder

--- src/elements/Info.jsx
+++ src/elements/Info.jsx
@@ -12,11 +12,11 @@ const Info = () =>{
                <tr>
                    <td className="pl-5 pr-5"><Personinfo PersonalInfo={["Age: 18", "Tasks:", " Builden the WebApp,", "Setting up the Arduino,", "Designing the Charts with Chartjs", "Role: Programmer"]} Name={"Nick Hillmann"} Picture={"/src/assets/Nick03.jpeg"}></Personinfo>
                    </td>
-                   <td className="pl-5 pr-5"><Personinfo PersonalInfo={["Age: 18", "Tasks:", "Dokumentation,", "Prognosefunktion (Berechnung),", "Design Templates", "Role: Teamleader"]} Name={"Jarno Calise"} Picture={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/2017-05-14_NRW_Landtagswahl_by_Olaf_Kosinsky-116.jpg/220px-2017-05-14_NRW_Landtagswahl_by_Olaf_Kosinsky-116.jpg"}></Personinfo>
+                   <td className="pl-5 pr-5"><Personinfo PersonalInfo={["Age: 18", "Tasks:", "Dokumentation,", "Prognosefunktion (Berechnung),", "Design Templates", "Role: Teamleader"]} Name={"Jarno Calise"} Picture={"/src/assets/JarnoProf.jpeg"}></Personinfo>
                    </td>
                </tr>
                <tr>
-                   <td className="pl-5 pr-5"><Personinfo PersonalInfo={["Age: 21", "Tasks:", "Marketing,", "Design,", "Investor", "Role: Finazmanger"]} Name={"Marc Müller"} Picture={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/2017-05-14_NRW_Landtagswahl_by_Olaf_Kosinsky-116.jpg/220px-2017-05-14_NRW_Landtagswahl_by_Olaf_Kosinsky-116.jpg"}></Personinfo>
+                   <td className="pl-5 pr-5"><Personinfo PersonalInfo={["Age: 21", "Tasks:", "Marketing,", "Design,", "Investor", "Role: Finazmanger"]} Name={"Marc Müller"} Picture={"/src/assets/MarcProf.jpeg"}></Personinfo>
                    </td>
                    <td className="pl-5 pr-5"><Personinfo PersonalInfo={["Age: 18,", "Tasks:" ,"Cleaning code,","Coding the App Layout", "Code debugging", "Role in this team: programmer"]} Name={"Fabian Holler"} Picture={"/src/assets/FabianProf.png"}></Personinfo>
                    </td>
